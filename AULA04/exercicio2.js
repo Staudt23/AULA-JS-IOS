7285881db4dfb61aff7c76bfb74be403c36885ce
staudt23 <luckyzadinha2007@gmail.com>
2025-10-14 12:07:46 -0300

    aulas js

---
+++ AULA04/exercicio2.js
@@ -0,0 +1,35 @@
+let dia_da_semana = prompt(`dia da semanha`)
+
+switch (dia_da_semana) {
+  case "1":
+    alert("segunda-feira");
+
+    break;
+  case "2":
+    alert("terça-feira");
+
+    break;
+  case "3":
+    alert("quarta-feira");
+
+    break;
+  case "4":
+    alert("quinta-feira");
+
+    break;
+  case "5":
+    alert("sexta-feira");
+
+    break;
+  case "6":
+    alert("sabado");
+
+    break;
+  case "7":
+    alert("domingo");
+
+    break;
+  default:
+    alert("feriado");
+    break;
+}
